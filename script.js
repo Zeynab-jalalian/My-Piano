0e19ac69d2eecb76762736537cd90cd991891125
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-23 12:44:11 +0330

    finish

--- script.js
+++ script.js
@@ -9,9 +9,20 @@ function playSound(key){ //تابع پخش صدای کلید
         key.classList.remove('active') //حذف کلاس پس از 0.5 ثانیه
     },500)
 }
-
 pianoKeys.forEach(function(key){ //حلقه ای برای تک تک کلیدها
     key.addEventListener('click',function(){ //در صورت کلیک شدن کلیدی
         playSound(key); //تابع مربوطه اجرا شود
     })
+})
+window.addEventListener('keydown',function(event){ //فشردن کلید کیبورد
+    const key=document.querySelector(`.key[data-key="${event.key}"]`); //کلید فشرده شده باید در اچ تی ام ال باشد
+    if(key){
+        playSound(key); //اگر شرط درست بود اجرا شه
+    }
+})
+window.addEventListener('keyup',function(event){ //فشردن کلید کیبورد
+    const key=document.querySelector(`.key[data-key="${event.key}"]`); //کلید فشرده شده باید در اچ تی ام ال باشد
+    if(key){
+       key.classList.remove('active'); //اگر شرط درست بود کلاس حذف شود
+    }
 })
